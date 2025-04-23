b5da2ece8d82406d59ba50e43dd4d95fd59ec16d
Michael <michael.bobrink@freenet.de>
2025-04-23 11:05:50 +0200

    Pushing Scripts into repository

---
+++ Assets/Scripts/Direction.cs
@@ -0,0 +1,8 @@
+﻿public enum Direction {
+    foreward,  // z+ direction
+    right,  // +x direction
+    backwards,   // -z direction
+    left,   // -x direction
+    up,     // +y direction
+    down    // -y direction
+};
